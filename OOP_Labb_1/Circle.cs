787a41101be764323aa706b6414ffada31937ed0
sidstedt <daniel.sidstedt@gmail.com>
2024-10-22 18:38:32 +0200

    Sorted out the classes to their own .cs-files

---
+++ OOP_Labb_1/Circle.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Labb_1
+{
+    internal class Circle
+    {
+        // creating variables for the class and set value to _Pi
+        public int _Radius;
+        public float _Pi = 3.141f;
+        // constructor
+        public Circle(int radius)
+        {
+            // copy the input value to the class variable
+            _Radius = radius;
+        }
+        // Method to calculate the area
+        public float GetArea()
+        {
+            // return the calculated variable
+            return _Pi * _Radius * _Radius;
+        }
+        // Method to calculate circumference
+        public float GetCircum()
+        {
+            return 2 * _Pi * _Radius;
+        }
+        // Method to calculate volume of spehere
+        public float GetVolumeSpehere()
+        {
+            return 4f / 3f * _Pi * (_Radius * _Radius * _Radius);
+        }
+        // method to calculate area of sphere
+        public float GetAreaSphere()
+        {
+            return 4 * _Pi * (_Radius * _Radius);
+        }
+    }
+}
---
+++ OOP_Labb_1/Triangle.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Labb_1
+{
+    internal class Triangle
+    {
+        // instanciate new variables
+        public int _CathethusA;
+        public int _CathethusB;
+        // Constructor to store values in the class
+        public Triangle(int cathethusA, int cathethusB)
+        {
+            _CathethusA = cathethusA;
+            _CathethusB = cathethusB;
+        }
+        // method to calculate are
+        public float GetArea()
+        {
+            return (_CathethusA * _CathethusB) / 2;
+        }
+        // method to calculate the length of the hypotenuse
+        public double GetHypoRight()
+        {
+            return Math.Sqrt(Math.Pow(_CathethusA, 2) + Math.Pow(_CathethusB, 2));
+        }
+    }
+}
